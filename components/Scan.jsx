858a10994011bee5d383ae5c62efc1cb90b7b290
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-06-11 13:35:44 +0000

    Sun, Jun 11, 2023, 1:35 PM +00:00

--- components/Scan.jsx
+++ components/Scan.jsx
@@ -215,7 +215,7 @@ function Scan() {
         const { name, time } = studentInfo;
         setData(`${name} | ${time}`);
         setScannedCodes(new Set(scannedCodes.add(code)));
-        updateBackgroundColor("#FFC300"); // Valid QR code, set background color to green
+        updateBackgroundColor("#32CD32"); // Valid QR code, set background color to green
       }
     } catch (e) {
       console.error("Error marking student as present: ", e);
