b0173f02c198afccb06f6efab2977afcd65ca8e3
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-06-13 10:03:24 +0000

    Tue, Jun 13, 2023, 10:03 AM +00:00

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>scans | Scanner</title>
+        <title>scans | scanner</title>
         <meta name="description" content="Welcome to Scans" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.jpeg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400&amp;display=swap"
           rel="stylesheet"
