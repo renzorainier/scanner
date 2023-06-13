d1777c00f021e6c2af6c17471a2aaae5139fa4b5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-06-13 09:16:50 +0000

    Tue, Jun 13, 2023, 9:16 AM +00:00

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(wav)$/,
+      use: {
+        loader: 'file-loader',
+      },
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPlugins([withImages], nextConfig);
